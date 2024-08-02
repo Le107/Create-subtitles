c4c5e1b766a1f99beeed02d258f34623dd873b04
Le107 <157118445+Le107@users.noreply.github.com>
2024-08-02 21:35:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pydub import AudioSegment
 from scipy.io import wavfile
 
-os.chdir('Sound')
+os.chdir('DialogEN')
 
 def output_duration(length):
     hours = length // 3600  # calculate in hours
@@ -21,5 +21,17 @@ for item in Path('.').glob('*.wav'):
     duration = len(audio) / 1000.0
     dur = '{:02}:{:02}:{:06.3f} '.format(hours, mins, duration)
     dur = str(dur).replace(".", ",")
-    print(item, dur)
-    print(item, dur, file=open('..\output.txt', 'a'))
+    name = str(item)
+    res = name.rstrip('.wav')
+    with open(r"..\Dialogs.txt", "r", encoding='utf-8') as fp:
+        lines = fp.readlines()
+        for row in lines:
+            if row.find(res) != -1:
+                idx = row.find('=')
+                dialog = row[idx + 1:]
+                print("1")
+                print("00:00:00,000 -->", dur)
+                print(dialog)
+                print('1', file=open(f'..\Srt\{res}.srt', 'a'))
+                print("00:00:00,000 -->", dur, file=open(f'..\Srt\{res}.srt', 'a'))
+                print(dialog, file=open(f'..\Srt\{res}.srt', 'a'))
