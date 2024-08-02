37e5134b792be83624a3629d7de92e2ca619cc9a
Le107 <157118445+Le107@users.noreply.github.com>
2024-08-02 17:38:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import os
+from pathlib import Path
+from pydub import AudioSegment
+from scipy.io import wavfile
+
+os.chdir('DialogEN')
+
+def output_duration(length):
+    hours = length // 3600  # calculate in hours
+    length %= 3600
+    mins = length // 60  # calculate in minutes
+    return hours, mins
+
+
+for item in Path('.').glob('*.wav'):
+    audio = AudioSegment.from_wav(item)
+    sample_rate, data = wavfile.read(item)
+    len_data = len(data)
+    t = len_data / sample_rate
+    hours, mins = output_duration(int(t))
+    duration = len(audio) / 1000.0
+    dur = '{:02}:{:02}:{:06}'.format(hours, mins, duration)
+    dur = str(dur).replace(".", ",")
+    print(item, dur, file=open('output.txt', 'a'))
